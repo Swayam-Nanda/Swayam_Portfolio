a1737392e1d6c4d468b8a93ccd7f591c91f8ff93
Swayam-Nanda <nandaswayam00@gmail.com>
2025-07-18 19:48:02 +0530

    Responsive

--- swayam_portfolio/settings.py
+++ swayam_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-s&)u!#$cycpz)!r6l%ql)b@k57y!@wk-g0f0hf5zoqi60is+q&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['swayam-portfolio.onrender.com']
+ALLOWED_HOSTS = ['swayam-portfolio.onrender.com' , 'localhost', '127.0.0.1']
 
 
 # Application definition
